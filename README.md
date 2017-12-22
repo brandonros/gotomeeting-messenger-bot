5074cbe8a743bdfe1faa8aed042a979c795f7e6d
Brandon Ros <brandonros1@gmail.com>
2017-12-22 00:07:03 -0500

    pretend that i know what i am doing

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Because the world needs another Slack...
 
 ## Usage
 
-Run `node bot.js emailAddress password`
+Run `node run.js emailAddress password`
---
+++ run.js
@@ -0,0 +1,23 @@
+var Bot = require('./lib/Bot.js');
+
+(async function() {
+  var emailAddress = process.argv[2];
+  var password = process.argv[3];
+
+  if (!emailAddress || !password) {
+    console.error('usage: bot emailAddress password');
+    process.exit(1);
+  }
+  
+  var bot = new Bot(emailAddress, password);
+
+  await bot.login()
+
+  await bot.run();
+})();
+
+process.on('unhandledRejection', function(err) {
+ console.error(err.stack);
+ process.exit(1);
+});
+
